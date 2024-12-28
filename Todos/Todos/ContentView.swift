f9a668304baf2cd98b03b97843982f629198e594
Yuto Hayashi <yuto.884.0829@icloud.com>
2024-12-28 13:08:59 +0900

    プロジェクトファイル追加

---
+++ Todos/Todos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Todos
+//
+//  Created by 林悠斗 on 2024/12/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Todos/Todos/TodosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodosApp.swift
+//  Todos
+//
+//  Created by 林悠斗 on 2024/12/28.
+//
+
+import SwiftUI
+
+@main
+struct TodosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
