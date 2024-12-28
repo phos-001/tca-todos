0e089b5cace3ed4959ea341975bb526c36af7e91
Yuto Hayashi <yuto.884.0829@icloud.com>
2024-12-28 22:27:11 +0900

    編集機能を実装

--- Todos/Todos/ContentView.swift
+++ Todos/Todos/ContentView.swift
@@ -34,9 +34,13 @@ struct TodosReducer {
     enum Action: BindableAction, Sendable {
         case addTodoButtonTapped
         case binding(BindingAction<State>)
+        case delete(IndexSet)
+        case move(IndexSet, Int)
+        case sortCompletedTodos
         case todos(IdentifiedActionOf<TodoReducer>)
     }
 
+    @Dependency(\.continuousClock) var clock
     @Dependency(\.uuid) var uuid
 
     var body: some Reducer<State, Action> {
@@ -47,6 +51,31 @@ struct TodosReducer {
                 state.todos.insert(TodoReducer.State(id: self.uuid()), at: 0)
                 return .none
             case .binding:
+                return .none
+            case let .delete(indexSet):
+                let filteredTodos = state.filteredTodos
+                indexSet.forEach {
+                    state.todos.remove(id: filteredTodos[$0].id)
+                }
+                return .none
+            case var .move(source, destination):
+                if state.filter == .completed {
+                    source = IndexSet(
+                        source
+                            .map { state.filteredTodos[$0] }
+                            .compactMap { state.todos.index(id: $0.id) })
+                    destination = (
+                        destination < state.filteredTodos.endIndex
+                        ? state.todos.index(id: state.filteredTodos[destination].id)
+                        : state.todos.endIndex) ?? destination
+                }
+                state.todos.move(fromOffsets: source, toOffset: destination)
+                return .run { send in
+                    try await self.clock.sleep(for: .microseconds(100))
+                    await send(.sortCompletedTodos)
+                }
+            case .sortCompletedTodos:
+                state.todos.sort { $1.isComplete && !$0.isComplete }
                 return .none
             case .todos:
                 return .none
@@ -71,18 +100,31 @@ struct ContentView: View {
                 }
                 .pickerStyle(.segmented)
                 .padding(.horizontal)
-
-                List(store.scope(state: \.filteredTodos,
-                                 action: \.todos)) { store in
-                    TodoView(store: store)
+                
+                List() {
+                    ForEach(store.scope(state: \.filteredTodos, action: \.todos)) { store in
+                        TodoView(store: store)
+                    }
+                    .onDelete { store.send(.delete($0)) }
+                    .onMove { store.send(.move($0, $1)) }
                 }
             }
             .navigationTitle("Todoリスト")
             .navigationBarItems(trailing: HStack {
+                Button.init(action: {
+                    store.editMode = store.editMode.isEditing ? .inactive : .active
+                }, label: {
+                    if store.editMode.isEditing {
+                        Text("完了")
+                    } else {
+                        Text("編集")
+                    }
+                })
                 Button("追加") {
                     store.send(.addTodoButtonTapped, animation: .default)
                 }
             })
+            .environment(\.editMode, $store.editMode)
         }
     }
 }
